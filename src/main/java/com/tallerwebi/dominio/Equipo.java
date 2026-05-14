19c6fc898d11bcbf2063e7f95b4bc27d251c35b6
Slpz12 <sofia-lopez12@outlook.com>
2026-05-14 20:06:08 -0300

    agego entidades base Jugador, Equipo y Posicion como enum

--- src/main/java/com/tallerwebi/dominio/Equipo.java
+++ src/main/java/com/tallerwebi/dominio/Equipo.java
@@ -1,7 +1,5 @@
 package com.tallerwebi.dominio;
 
-import net.bytebuddy.asm.Advice;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
