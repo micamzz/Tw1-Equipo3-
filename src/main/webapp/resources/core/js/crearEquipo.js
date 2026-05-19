d61e3d4b01bd180397810808a2b98875f49f3cee
Micaela <micamazza@hotmail.com>
2026-05-19 20:18:07 -0300

    Se eliminaron espacios en JS que hacian q falle la CI

--- src/main/webapp/resources/core/js/crearEquipo.js
+++ src/main/webapp/resources/core/js/crearEquipo.js
@@ -4,11 +4,11 @@ const botonSiguiente = document.querySelector(".boton-equipo");
 /* Para que el boton SIGUIENTE se active solo al completar el input */
 nombreIngresadoInput.addEventListener("input", () => {
 
-    const textoIngresado = nombreIngresadoInput.value.trim();
+  const textoIngresado = nombreIngresadoInput.value.trim();
 
-    if (textoIngresado.length < 4) {
-        botonSiguiente.disabled = true;
-    } else {
-        botonSiguiente.disabled = false;
-    }
+  if (textoIngresado.length < 4) {
+    botonSiguiente.disabled = true;
+  } else {
+    botonSiguiente.disabled = false;
+  }
 });
