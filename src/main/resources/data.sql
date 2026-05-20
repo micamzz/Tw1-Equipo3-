10c622c4db067a384282f3038ec1bc7367d16171
Valentin Virzi <v.virzi@hotmail.com>
2026-05-20 16:52:42 -0300

    Update Jugador insertion in data.sql to include lesionado attribute; refactor ServicioMercadoTest for consistent spacing and formatting

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
-INSERT INTO Jugador(nombre,apellido, posicion, precio, dni)VALUES
-('Luka', 'Doncic', 'BASE', 5200000, 1),
-('LeBron', 'James', 'ALERO', 6000000, 2),
-('Kevin', 'Durant', 'ALERO', 5000000, 3),
-('Nikola','Jokic', 'PIVOT', 5500000, 4),
-('Joel', 'Embiid', 'PIVOT', 5300000, 5);
+INSERT INTO Jugador(nombre,apellido, posicion, precio, dni, lesionado)VALUES
+('Luka', 'Doncic', 'BASE', 5200000, 1, false),
+('LeBron', 'James', 'ALERO', 6000000, 2, false),
+('Kevin', 'Durant', 'ALERO', 5000000, 3, false),
+('Nikola','Jokic', 'PIVOT', 5500000, 4, false),
+('Joel', 'Embiid', 'PIVOT', 5300000, 5, false);
--- src/test/java/com/tallerwebi/dominio/ServicioMercadoTest.java
+++ src/test/java/com/tallerwebi/dominio/ServicioMercadoTest.java
@@ -1,7 +1,6 @@
 package com.tallerwebi.dominio;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import java.util.Arrays;
@@ -12,65 +11,66 @@ import static org.mockito.Mockito.when;
 public class ServicioMercadoTest {
     @Test
     public void queBuscarPorNombreDevuelvaJugadoresQueCoincidan() {
-        RepositorioJugador repositorioMock= mock(RepositorioJugador.class);
+        RepositorioJugador repositorioMock = mock(RepositorioJugador.class);
 
         Jugador jugador = new Jugador();
         jugador.setNombre("Stephen");
         jugador.setApellido("Curry");
-        when(repositorioMock.buscarJugadores(null,"Stephen")).thenReturn(Arrays.asList(jugador));
+        when(repositorioMock.buscarJugadores(null, "Stephen")).thenReturn(Arrays.asList(jugador));
 
         ServicioMercado servicio = new ServicioMercadoImpl(repositorioMock);
 
         List<Jugador> resultado = servicio.obtenerJugadores(null, "Stephen");
         assertThat(resultado, not(empty()));
-        assertThat(resultado.get(0).getNombre(),equalTo("Stephen"));
+        assertThat(resultado.get(0).getNombre(), equalTo("Stephen"));
 
     }
+
     @Test
-    public void queBuscarAleroDeveulvaJugadoresConPosicionAlero(){
-        RepositorioJugador repositorioMock= mock(RepositorioJugador.class);
+    public void queBuscarAleroDeveulvaJugadoresConPosicionAlero() {
+        RepositorioJugador repositorioMock = mock(RepositorioJugador.class);
         Jugador jugador = new Jugador();
         jugador.setNombre("LeBron");
         jugador.setApellido("James");
         jugador.setPosicion(Posicion.ALERO);
-        when(repositorioMock.buscarJugadores(Posicion.ALERO,null)).thenReturn(Arrays.asList(jugador));
+        when(repositorioMock.buscarJugadores(Posicion.ALERO, null)).thenReturn(Arrays.asList(jugador));
 
         ServicioMercado servicio = new ServicioMercadoImpl(repositorioMock);
 
         List<Jugador> resultado = servicio.buscarAlero();
         assertThat(resultado, not(empty()));
-        assertThat(resultado.get(0).getPosicion(),equalTo(Posicion.ALERO));
+        assertThat(resultado.get(0).getPosicion(), equalTo(Posicion.ALERO));
     }
 
     @Test
-    public void queBuscarPivotDevuelvaJugadoresConPosicionPivot(){
-        RepositorioJugador repositorioMock= mock(RepositorioJugador.class);
+    public void queBuscarPivotDevuelvaJugadoresConPosicionPivot() {
+        RepositorioJugador repositorioMock = mock(RepositorioJugador.class);
         Jugador jugador = new Jugador();
         jugador.setNombre("Nikola");
         jugador.setApellido("Jokic");
         jugador.setPosicion(Posicion.PIVOT);
-        when(repositorioMock.buscarJugadores(Posicion.PIVOT,null)).thenReturn(Arrays.asList(jugador));
+        when(repositorioMock.buscarJugadores(Posicion.PIVOT, null)).thenReturn(Arrays.asList(jugador));
 
         ServicioMercado servicio = new ServicioMercadoImpl(repositorioMock);
 
         List<Jugador> resultado = servicio.buscarPivot();
         assertThat(resultado, not(empty()));
-        assertThat(resultado.get(0).getPosicion(),equalTo(Posicion.PIVOT));
+        assertThat(resultado.get(0).getPosicion(), equalTo(Posicion.PIVOT));
     }
 
     @Test
-    public void queBuscarBaseDevuevlaJugadoresConPosicionBase(){
-        RepositorioJugador repositorioMock= mock(RepositorioJugador.class);
+    public void queBuscarBaseDevuevlaJugadoresConPosicionBase() {
+        RepositorioJugador repositorioMock = mock(RepositorioJugador.class);
         Jugador jugador = new Jugador();
         jugador.setNombre("Stephen");
         jugador.setApellido("Curry");
         jugador.setPosicion(Posicion.BASE);
-        when(repositorioMock.buscarJugadores(Posicion.BASE,null)).thenReturn(Arrays.asList(jugador));
+        when(repositorioMock.buscarJugadores(Posicion.BASE, null)).thenReturn(Arrays.asList(jugador));
 
         ServicioMercado servicio = new ServicioMercadoImpl(repositorioMock);
 
         List<Jugador> resultado = servicio.buscarBase();
         assertThat(resultado, not(empty()));
-        assertThat(resultado.get(0).getPosicion(),equalTo(Posicion.BASE));
+        assertThat(resultado.get(0).getPosicion(), equalTo(Posicion.BASE));
     }
 }
