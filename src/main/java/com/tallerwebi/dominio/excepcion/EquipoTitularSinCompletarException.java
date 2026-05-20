678a9df889f29bbd41c4d519f8f331a0a0cd1ea7
Micaela <micamazza@hotmail.com>
2026-05-20 00:45:22 -0300

    se agregaron 2 excepciones

---
+++ src/main/java/com/tallerwebi/dominio/excepcion/EquipoTitularSinCompletarException.java
@@ -0,0 +1,7 @@
+package com.tallerwebi.dominio.excepcion;
+
+public class EquipoTitularSinCompletarException extends RuntimeException {
+    public EquipoTitularSinCompletarException(String message) {
+        super(message);
+    }
+}
