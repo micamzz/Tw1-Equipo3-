2a93789faa62f019779f1c5cfd8444498b2a9021
Micaela <micamazza@hotmail.com>
2026-05-23 20:16:12 -0300

    Modificaciones en html crear equipo y seleccionar jugadores viejo- y se agregan de nuevo los constructores a dos excepciones

--- src/main/java/com/tallerwebi/dominio/excepcion/PresupuestoInsuficienteException.java
+++ src/main/java/com/tallerwebi/dominio/excepcion/PresupuestoInsuficienteException.java
@@ -3,5 +3,7 @@ package com.tallerwebi.dominio.excepcion;
 public class PresupuestoInsuficienteException extends Exception {
     private static final long serialVersionUID = 1L;
 
-
+    public PresupuestoInsuficienteException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/tallerwebi/dominio/excepcion/elJugadorYaExisteEnElEquipoException.java
+++ src/main/java/com/tallerwebi/dominio/excepcion/elJugadorYaExisteEnElEquipoException.java
@@ -3,5 +3,8 @@ package com.tallerwebi.dominio.excepcion;
 public class elJugadorYaExisteEnElEquipoException extends Exception {
 
     private static final long serialVersionUID = 1L;
-    
+
+    public elJugadorYaExisteEnElEquipoException(String message) {
+        super(message);
+    }
 }
