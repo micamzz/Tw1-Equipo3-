92ec6d5bd86a306573d05a0030587f050026f254
Sabrina Federico <sabrinafederico88@gmail.com>
2026-06-03 17:35:14 -0300

    Se agregaron validaciones en el controlador torneo, tests y exceptions

---
+++ src/main/java/com/tallerwebi/dominio/excepcion/NombreDeTorneoEnBlancoException.java
@@ -0,0 +1,7 @@
+package com.tallerwebi.dominio.excepcion;
+
+public class NombreDeTorneoEnBlancoException extends Exception {
+    public NombreDeTorneoEnBlancoException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ src/main/java/com/tallerwebi/dominio/excepcion/TorneoNoEncontradoException.java
@@ -0,0 +1,6 @@
+package com.tallerwebi.dominio.excepcion;
+
+public class TorneoNoEncontradoException extends Exception {
+    public TorneoNoEncontradoException(String mensaje) { super(mensaje);
+    }
+}
