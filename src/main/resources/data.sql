befecb9fa74bec407eb7926dca3f6e9c585a4a89
Micaela <micamazza@hotmail.com>
2026-06-07 00:53:21 -0300

    Actualizacion de datos en data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,15 @@
 INSERT INTO Usuario(id, email, password, rol, activo)
 VALUES (null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
 
--- ─── JUGADORES ───
+-- JUGADORES
 INSERT INTO Jugador(id, nombre, apellido, posicion, precio, dni, foto, altura, peso, nacionalidad, edad,
                     fechaNacimiento, draft, experiencia)
 VALUES
--- Golden State Warriors (1-6)
+-- Golden State Warriors
 (1, 'Stephen', 'Curry', 'BASE', 175000, 1, 'https://cdn.nba.com/headshots/nba/latest/1040x760/201939.png',
  '6''2" (1.88m)', '185lb (84kg)', 'USA', 37, 'March 14, 1988', '2009 R1 Pick 7', 15),
-(2, 'Klay', 'Thompson', 'ALERO', 130000, 2, 'https://cdn.nba.com/headshots/nba/latest/1040x760/202691.png',
- '6''6" (1.98m)', '215lb (98kg)', 'USA', 34, 'February 8, 1990', '2011 R1 Pick 11', 13),
+(2, 'Moses', 'Moody', 'ALERO', 85000, 2, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630541.png',
+ '6''6" (1.98m)', '205lb (93kg)', 'USA', 22, 'May 31, 2002', '2021 R1 Pick 14', 3),
 (3, 'Draymond', 'Green', 'PIVOT', 120000, 3, 'https://cdn.nba.com/headshots/nba/latest/1040x760/203110.png',
  '6''6" (1.98m)', '230lb (104kg)', 'USA', 34, 'March 4, 1990', '2012 R2 Pick 35', 12),
 (4, 'Andrew', 'Wiggins', 'ALERO', 110000, 4, 'https://cdn.nba.com/headshots/nba/latest/1040x760/203952.png',
@@ -19,7 +19,7 @@ VALUES
 (6, 'Kevon', 'Looney', 'PIVOT', 80000, 6, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1626172.png',
  '6''9" (2.06m)', '222lb (101kg)', 'USA', 28, 'February 6, 1996', '2015 R1 Pick 30', 9),
 
--- Boston Celtics (7-12)
+-- Boston Celtics
 (7, 'Jayson', 'Tatum', 'ALERO', 160000, 7, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1628369.png',
  '6''8" (2.03m)', '210lb (95kg)', 'USA', 26, 'March 3, 1998', '2017 R1 Pick 3', 7),
 (8, 'Jaylen', 'Brown', 'ALERO', 145000, 8, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1627759.png',
@@ -33,7 +33,7 @@ VALUES
 (12, 'Payton', 'Pritchard', 'BASE', 75000, 12, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1630202.png',
  '6''1" (1.85m)', '195lb (88kg)', 'USA', 26, 'January 28, 1998', '2020 R1 Pick 26', 4),
 
--- Los Angeles Lakers (13-18)
+-- Los Angeles Lakers
 (13, 'LeBron', 'James', 'ALERO', 100000, 13, 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
  '6''9" (2.06m)', '250lb (113kg)', 'USA', 40, 'December 30, 1984', '2003 R1 Pick 1', 21),
 (14, 'Anthony', 'Davis', 'PIVOT', 165000, 14, 'https://cdn.nba.com/headshots/nba/latest/1040x760/203076.png',
@@ -47,7 +47,7 @@ VALUES
 (18, 'Jarred', 'Vanderbilt', 'PIVOT', 80000, 18, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629012.png',
  '6''9" (2.06m)', '214lb (97kg)', 'USA', 25, 'April 3, 1999', '2018 R2 Pick 41', 6),
 
--- Miami Heat (19-24)
+-- Miami Heat
 (19, 'Jimmy', 'Butler', 'ALERO', 125000, 19, 'https://cdn.nba.com/headshots/nba/latest/1040x760/202710.png',
  '6''7" (2.01m)', '230lb (104kg)', 'USA', 35, 'September 14, 1989', '2011 R1 Pick 30', 13),
 (20, 'Bam', 'Adebayo', 'PIVOT', 120000, 20, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1628389.png',
@@ -56,25 +56,25 @@ VALUES
  '6''4" (1.93m)', '195lb (88kg)', 'USA', 24, 'January 20, 2000', '2019 R1 Pick 13', 5),
 (22, 'Kevin', 'Love', 'PIVOT', 85000, 22, 'https://cdn.nba.com/headshots/nba/latest/1040x760/201567.png',
  '6''8" (2.03m)', '251lb (114kg)', 'USA', 36, 'September 7, 1988', '2008 R1 Pick 5', 16),
-(23, 'Caleb', 'Martin', 'ALERO', 75000, 23, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629637.png',
- '6''5" (1.96m)', '205lb (93kg)', 'USA', 28, 'September 28, 1995', '2019 UD', 5),
-(24, 'Terry', 'Rozier', 'BASE', 105000, 24, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1626179.png',
- '6''2" (1.88m)', '190lb (86kg)', 'USA', 30, 'March 17, 1994', '2015 R1 Pick 16', 9),
+(23, 'Duncan', 'Robinson', 'ALERO', 90000, 23, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629130.png',
+ '6''7" (2.01m)', '215lb (98kg)', 'USA', 30, 'April 22, 1994', '2018 UD', 6),
+(24, 'Nikola', 'Jovic', 'BASE', 75000, 24, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1631107.png',
+ '6''10" (2.08m)', '215lb (98kg)', 'Serbia', 21, 'June 8, 2003', '2022 R1 Pick 27', 2),
 
--- Jugadores libres (25-28)
-(25, 'Luka', 'Doncic', 'BASE', 220000, 25, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629029.png',
- '6''7" (2.01m)', '230lb (104kg)', 'Eslovenia', 25, 'February 28, 1999', '2018 R1 Pick 3', 6),
-(26, 'Nikola', 'Jokic', 'PIVOT', 210000, 26, 'https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png',
- '6''11" (2.11m)', '284lb (129kg)', 'Serbia', 30, 'February 19, 1995', '2014 R2 Pick 41', 10),
+-- Jugadores libres
+(25, 'Joel', 'Embiid', 'PIVOT', 200000, 25, 'https://cdn.nba.com/headshots/nba/latest/1040x760/203954.png',
+ '7''0" (2.13m)', '280lb (127kg)', 'Camerun', 30, 'March 16, 1994', '2014 R1 Pick 3', 9),
+(26, 'Damian', 'Lillard', 'BASE', 185000, 26, 'https://cdn.nba.com/headshots/nba/latest/1040x760/203081.png',
+ '6''2" (1.88m)', '195lb (88kg)', 'USA', 34, 'July 15, 1990', '2012 R1 Pick 6', 12),
 (27, 'Ja', 'Morant', 'BASE', 180000, 27, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629630.png',
  '6''2" (1.88m)', '174lb (79kg)', 'USA', 25, 'August 10, 1999', '2019 R1 Pick 2', 5),
-(28, 'Kawhi', 'Leonard', 'ALERO', 170000, 28, 'https://cdn.nba.com/headshots/nba/latest/1040x760/202695.png',
- '6''7" (2.01m)', '225lb (102kg)', 'USA', 33, 'June 29, 1991', '2011 R1 Pick 15', 13);
+(28, 'Karl-Anthony', 'Towns', 'PIVOT', 150000, 28, 'https://cdn.nba.com/headshots/nba/latest/1040x760/1626157.png',
+ '7''0" (2.13m)', '270lb (122kg)', 'USA', 29, 'November 15, 1995', '2015 R1 Pick 1', 9);
 
--- ─── RENDIMIENTOS ───
+-- RENDIMIENTOS
 INSERT INTO RendimientoJugador(jugador_id, puntos, rebotes, asistencias, robos, bloqueos, perdidas)
 VALUES (1, 26, 5, 6, 1, 0, 2),
-       (2, 18, 4, 2, 1, 0, 1),
+       (2, 14, 3, 2, 1, 0, 1),
        (3, 9, 7, 7, 1, 1, 2),
        (4, 17, 5, 2, 1, 0, 1),
        (5, 16, 5, 2, 1, 0, 1),
@@ -95,21 +95,21 @@ VALUES (1, 26, 5, 6, 1, 0, 2),
        (20, 19, 10, 3, 1, 1, 2),
        (21, 21, 4, 4, 1, 0, 2),
        (22, 8, 6, 2, 0, 0, 1),
-       (23, 9, 4, 2, 1, 0, 1),
-       (24, 19, 4, 4, 1, 0, 2),
-       (25, 28, 8, 9, 1, 0, 4),
-       (26, 26, 13, 9, 1, 1, 3),
+       (23, 14, 4, 2, 1, 0, 1),
+       (24, 10, 4, 3, 1, 0, 1),
+       (25, 30, 11, 5, 1, 2, 3),
+       (26, 24, 4, 7, 1, 0, 3),
        (27, 20, 5, 8, 1, 0, 3),
-       (28, 22, 6, 3, 2, 1, 1);
+       (28, 21, 8, 4, 1, 1, 2);
 
--- ─── EQUIPOS NBA  ───
+-- EQUIPOS NBA
 INSERT INTO EquipoNBA(id, nombre, escudoImagen)
 VALUES (1, 'Golden State Warriors', 'https://cdn.nba.com/logos/nba/1610612744/global/L/logo.svg'),
        (2, 'Boston Celtics', 'https://cdn.nba.com/logos/nba/1610612738/global/L/logo.svg'),
        (3, 'Los Angeles Lakers', 'https://cdn.nba.com/logos/nba/1610612747/global/L/logo.svg'),
        (4, 'Miami Heat', 'https://cdn.nba.com/logos/nba/1610612748/global/L/logo.svg');
 
--- ─── ASIGNACIONES EQUIPO-JUGADOR ───
+-- ASIGNACIONES EQUIPO-JUGADOR
 INSERT INTO EquipoNBAJugador(equipoNBA_id, jugador_id)
 VALUES
 -- Golden State Warriors
@@ -140,6 +140,8 @@ VALUES
 (4, 22),
 (4, 23),
 (4, 24);
+
+-- TORNEO
 INSERT INTO `tw1`.`torneo`
 (`id`,
  `fechaFin`,
