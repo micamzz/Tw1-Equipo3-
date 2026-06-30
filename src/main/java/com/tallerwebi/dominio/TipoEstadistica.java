d27293dc33dc61323f8989e6e114d65a5f3190fa
Sabrina Federico <sabrinafederico88@gmail.com>
2026-06-30 20:43:49 -0300

    Se acomodo la forma en que se muestra el tiempo en cronologia (no se mostraban los segundos si eran :00) se agrego descripcion del enum tipo estadistica para que en la cronologia se muestren mas amigables a la vista. se mejoro el estilo de la vista de carga de estadisticas

--- src/main/java/com/tallerwebi/dominio/TipoEstadistica.java
+++ src/main/java/com/tallerwebi/dominio/TipoEstadistica.java
@@ -2,14 +2,26 @@ package com.tallerwebi.dominio;
 
 
 public enum TipoEstadistica {
-    DOBLE,
-    TRIPLE,
-    TIRO_LIBRE,
-    REBOTE,
-    ASISTENCIA,
-    ROBO,
-    TAPA,
-    PERDIDA,
-    FALTA_PERSONAL
+    DOBLE ("+ 2 puntos"),
+    TRIPLE ("+ 3 puntos"),
+    TIRO_LIBRE ("+ 1 punto"),
+    REBOTE ("Rebote"),
+    ASISTENCIA ("Asistencia"),
+    ROBO ("Recuperación"),
+    TAPA ("Tapa"),
+    PERDIDA ("Perdida"),
+    FALTA_PERSONAL ("Falta personal");
+
+    private final String descripcion;
+
+    TipoEstadistica(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+
 
 }
